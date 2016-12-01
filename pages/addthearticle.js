fdd89fb8b8fa8b6eb43f4b5aa37dc2b86a7a3d5d
Baird-weng <644672334@qq.com>
2016-12-01 19:36:34 +0800

    增加文章发表

--- pages/addthearticle.js
+++ pages/addthearticle.js
@@ -2,7 +2,8 @@
  * Created by baird on 2016/11/23.
  */
 
-var index = 0;
+var index = -1;
+var images = new Array();
 /*添加事件*/
 $("#subbtn").on('click',function (){
    var input = $("#account").val();
@@ -25,6 +26,7 @@ $("#addtext").on('click',function (){
     var html = '<div class="list-block"><ul><div class="item-content"><div class="item-inner"><div class="item-input"><textarea id=' + '"'+text_id+ '">' + '</textarea></div></div></div></ul></div>';
     $("#mycontent").append(html);
 });
+
 /*添加图片*/
 $("#addpic").change(function (){
     var $file = $(this);
@@ -37,53 +39,81 @@ $("#addpic").change(function (){
     if(fileObj && fileObj.files && fileObj.files[0]){
         $("#mycontent").append(html_pic);
         var $img = $("#"+pic_id);
-        dataURL = windowURL.createObjectURL(fileObj.files[0]);
+        var files = fileObj.files[0];
+        var obj = {"index":index,"imgObj":files};
+        images.push(obj);
+        dataURL = windowURL.createObjectURL(files);
         $img.attr('src',dataURL);
     }
 })
 
-/*addrelease*/
+/*发布*/
 $("#addrelease").on('click',function (){
     var mycontent =  $("#mycontent").children();
     if (mycontent.length==0){
         $.toast("请添加内容");
     }
     else{
+        //添加文字
+       var uploadtextofart = "http://xggserve.com/xgg/uploadtextofart";
+       var texts = new Array();
        for(var i = 0;i<mycontent.length;i++){
-           var listid = "#"+(i+1);
+           var listid = "#"+(i);
            var obj = $(listid);
            var classname = obj.attr("class");
-           //图片
-           if (classname=="card-cover"){
-               var src = obj.attr("src");
-               // console.log(src);
-               $.ajax({
-                   type: "POST",
-                   url: "http://123.207.47.17/xgg/updatetheavatar",
-                   enctype: 'multipart/form-data',
-                   data: {
-                       img:src,
-                       "img":src,
-                       "token":"D82377E8-7F4E-929F-7AD5-858EDE572814",
-                       "usertoken":"9f91c90dc803f9c668b3c521de9399d1"
-                   },
-                   success: function (result) {
-
-                       console.log(result);
-                       // alert("Data Uploaded: ");
-                   },
-                   error:function (error) {
-                       // alert(error);
-                   }
-               });
-           }
-           //文字
-           else {
-
+           //获取文字内容。
+           if (classname!="card-cover"){
+               var index = i;
+               var value = obj.val();
+               var obj = {"index":i,"text":value};
+               texts.push(obj);
            }
        }
+        var json_texts = JSON.stringify(texts);
+            $.post(uploadtextofart,{
+                    uid:"3",
+                    title:"测试啦",
+                    content:json_texts
+                },
+                function(data){
+                    var result = data['result'];
+                    if (result == "1"){
+                        var articleid = data['articleid'];
+                        upimage(articleid);
+                    }
+                    else{
+                        var error = data['error'];
+                        $.toast(error);
+                    }
+                });
     }
 })
+/*上传图片*/
+function upimage(a_id){
+    var uploadURL = "http://xggserve.com/xgg/uploadpicofart";
+    for(var i = 0;i<images.length;i++){
+        //获取图片对象。
+        var obj = images[i];
+        //图片对象
+        var imgObj = obj["imgObj"];
+        //图片索引。排序。
+        var index = obj["index"];
+        var reader = new FileReader();
+        reader.readAsDataURL(imgObj);
+        reader.onload = function (){
+            var imgData = this.result; //base64数据
+            $.post(uploadURL,{
+                    uid:"3",
+                    articleid:a_id,
+                    index:index,
+                    img:imgData
+                },
+                function(data){
+                    console.log(data);
+                });
+        };
+    }
+}
 
 
 
